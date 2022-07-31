8b57fd145028f106ed0d1d4187ccb12fd03414bb
JeanmarcoLujan <mark.15@hotmail.es>
2022-07-31 11:58:20 -0500

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Show from "./components/Show";
 import Create from "./components/Create";
 import Edit from "./components/Edit";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
